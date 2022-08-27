e87dcd805acb9efdc3dfbb49ee914e13707f2b62
LSWarss <ls.warss98@gmail.com>
2022-08-27 18:43:52 +0200

    Added first chart - bar chart

--- iOS16-SwiftUI-Showdown/ChartsScreen.swift
+++ iOS16-SwiftUI-Showdown/ChartsScreen.swift
@@ -6,9 +6,39 @@
 //
 
 import SwiftUI
+import Charts
+
+struct Developer: Identifiable {
+    let id: UUID = UUID()
+    let name: String
+    let seniority: Double
+}
 
 struct ChartsScreen: View {
     var feature: Feature
+    
+    @State var team: [Developer] = [
+        Developer(name: "Łukasz", seniority: 2.5),
+        Developer(name: "Wiktor", seniority: 3.5),
+        Developer(name: "Maciek", seniority: 6),
+        Developer(name: "Spaghetti", seniority: 6.6),
+        Developer(name: "Mateusz", seniority: 4.7),
+        Developer(name: "Patryk", seniority: 0.2),
+        Developer(name: "Piotrek", seniority: 0.2),
+        Developer(name: "Alicja", seniority: 0.2),
+        Developer(name: "Kuba", seniority: 0.2),
+        Developer(name: "Seba", seniority: 0.2),
+        Developer(name: "Damian", seniority: 0.2),
+        Developer(name: "Hubert", seniority: 1.3),
+        Developer(name: "Kamil", seniority: 10.3),
+        Developer(name: "Marcin", seniority: 12.3),
+        Developer(name: "Piotr", seniority: 8.25),
+        Developer(name: "Rafał S", seniority: 2.9),
+        Developer(name: "Miłosz", seniority: 6.3),
+        Developer(name: "Rafał", seniority: 6.7),
+        Developer(name: "Tomek", seniority: 6.9),
+        Developer(name: "Robert", seniority: 6.0)
+    ]
     
     var body: some View {
         VStack {
@@ -18,8 +48,50 @@ struct ChartsScreen: View {
                 .multilineTextAlignment(.center)
             Spacer()
             
+            ScrollView {
+                barChart
+                
+            }
         }
         .navigationTitle(feature.title)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    var barChart: some View {
+        VStack {
+            Chart(team) {
+                BarMark(x: .value("Developer Name", $0.name),
+                        y: .value("Developer Seniority", $0.seniority))
+                .foregroundStyle(.orange.gradient)
+            }
+            .frame(height: 400)
+            
+            HStack {
+                Button {
+                    withAnimation {
+                        team = team.sorted { $0.seniority < $1.seniority }
+                    }
+                } label: {
+                    Text("Sort")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.green)
+                        .cornerRadius(10)
+                }
+                
+                Button {
+                    withAnimation {
+                        team = team.shuffled()
+                    }
+                } label: {
+                    Text("Shuffle")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.red)
+                        .cornerRadius(10)
+                }
+            }
+        }
     }
 }
 
