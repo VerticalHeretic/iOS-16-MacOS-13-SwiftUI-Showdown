34f46615d0bd06f4f7357c11da534b5e149b8dfd
LSWarss <ls.warss98@gmail.com>
2022-08-27 14:52:36 +0200

    Initial Commit

---
+++ iOS16-SwiftUI-Showdown/iOS16_SwiftUI_ShowdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS16_SwiftUI_ShowdownApp.swift
+//  iOS16-SwiftUI-Showdown
+//
+//  Created by Łukasz Stachnik on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS16_SwiftUI_ShowdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
